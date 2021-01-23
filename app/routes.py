f9fca28c54640cd36b0b3c8bfeebb9fac8ba3d99
Gerben van Rijswijk <gvrijswijk@gmail.com>
2021-01-23 12:18:19 +0100

    Naive first solution of part 2 of the Flask Mega-Tutorial 'Templates' implemented

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username': 'Gerben'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
