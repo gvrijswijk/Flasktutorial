64d71a865b9c9ab23040ed3f8090ac51a44d923b
Gerben van Rijswijk <gvrijswijk@gmail.com>
2021-01-31 22:24:06 +0100

    Added basis form and functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from config import Config
 
 # The __name__ variable passed to the Flask class is a Python predefined variable, which is set to the name of the module in which it is used. 
 app = Flask(__name__)
+
+# ??
+app.config.from_object(Config)
 
 # circular imports
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 #The routes are the different URLs that the application implements. In Flask, handlers for the application routes are written as Python functions, called view functions. View functions are mapped to one or more route URLs so that Flask knows what logic to execute when a client requests a given URL.
 
 from app import app
+from app.forms import LoginForm
 
 # A decorator modifies the function that follows it. A common pattern with decorators is to use them to register functions as callbacks for certain events. In this case, the @app.route decorator creates an association between the URL given as an argument and the function.
 @app.route('/')
@@ -20,3 +21,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
